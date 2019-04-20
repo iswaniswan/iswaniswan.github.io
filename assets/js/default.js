68389b50f9cb0c731c73a868e6bf04ca2ca93eab
iswaniswan <iswan1s71@gmail.com>
2019-04-20 10:25:29 +0700

    update menu & content

--- assets/js/default.js
+++ assets/js/default.js
@@ -20,20 +20,36 @@ $(document).ready(function(){
 
 	$('a.nav-link').each(function(){
 		$(this).on('click', function(){
-            $('a.nav-link').removeClass('active');
-            activateStyle1();
+			$('a.nav-link').removeClass('active');
+			var activate = $('#profile-header').hasClass('activate') ? '' : activateStyle1() ;
+			//var carousel = $('#carousel-scanned-image').length == 1 ? '' : activeCarousel() ;
+			componentLoader($(this).attr('data-target'));
             $(this).addClass('active');
-            loadProfileContent($(this).attr('data-target'));
 		});
 	});
 
 });
 
+function navJs(){
+	$('.fas.smaller').on('click', function(){
+		var target = $(this).attr('data-target');
+		$('#profile-menu a[data-target='+target+']').trigger('click');
+	});
+}
+
+function clearTooltip(){
+	$('div.tooltip').hide();
+}
+
 function activateStyle1(){
-    var activate = $('#profile-header').hasClass('activate') ? '' : $('#profile-header').addClass('activate') ;
-    var carousel = $('#profile-content').attr('display') == 'block' ? '' : $('#profile-content').fadeIn('slow');
+    $('#profile-header').addClass('activate') ;
+    $('#profile-content').fadeIn('slow');
 	$('#profile-name').appendTo('#profile-header .wrapper');
 	$('#profile-menu').appendTo('#profile-header .wrapper');
+}
+
+function activeCarousel(){
+	$('#carousel').load('component/carousel-scanned-image.html');
 }
 
 function copyToClipboard(element) {
@@ -44,6 +60,31 @@ function copyToClipboard(element) {
 	$temp.remove();
 }
 
-function loadProfileContent(content){
-    $('#profile-content .loadable').load(content+'.html');
+function componentLoader(content){
+	$('#track').load('component/'+content+'.html', function(response, status, xhr){
+		if( xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0) ){
+			if( content === 'education' ) tooltipLocation();
+			clearTooltip();
+			navJs();
+			console.log('success loading ' + content);
+		}
+	});
 }
+
+function tooltipLocation(){
+	$('#accordion-education button').each(function(){
+		var place = $(this).text();
+		$(this).tooltip({
+			html: true,
+			placement: 'right', 
+			title: '<a href="https://www.google.com/maps/d/embed?mid=1sa78AXwGoj2j48qsjvpCxSli6_6ElatR" target="_blank" class="maps-lnk">open my maps</a>',
+			delay: {'show': 300, 'hide': 500}
+		});
+	});
+}
+
+
+/* maps links
+portofolio-education-maps
+https://www.google.com/maps/d/embed?mid=1sa78AXwGoj2j48qsjvpCxSli6_6ElatR
+*/
