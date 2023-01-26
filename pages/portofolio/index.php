d640514a06eb10255e2460796ca6308fb684442c
iswanto <iswan1s71@gmail.com>
2023-01-27 05:26:25 +0700

    portofolio from old demo pages

---
+++ pages/portofolio/index.php
@@ -0,0 +1,148 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet"/>
+    <title>App Portofolio</title>
+</head>
+    <body>
+        <div class="container my-5">
+            <div class="row">                
+                <?php /** anjab */ ?>
+                <div class="col-12 mb-4">
+                    <div class="card h-100">
+                        <div id="carouselAnjab" class="carousel slide" data-bs-touch="false" data-bs-interval="false">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="images/anjab/anjab1.png" class="d-block w-100">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="images/anjab/anjab2.png" class="d-block w-100">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="images/anjab/anjab3.png" class="d-block w-100">
+                                </div>
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselAnjab" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselAnjab" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">ANJAB</h5>
+                            <p class="card-text">Aplikasi Analisis Jabatan dan Evaluasi Jabatan</p>
+                            <p class=""><a href="https://previewaplikasi.com/anjab-cianjurkab" target="_blank">Lihat</a></p>
+                        </div>
+                    </div>
+                </div>
+                <?php /** sofin */ ?>
+                <div class="col-12 mb-4">
+                    <div class="card h-100">
+                        <div id="carouselSofin" class="carousel slide" data-bs-touch="false" data-bs-interval="false">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="images/sofin/sofin1.png" class="d-block w-100">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="images/sofin/sofin2.png" class="d-block w-100">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="images/sofin/sofin3.png" class="d-block w-100">
+                                </div>
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselSofin" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselSofin" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">MLM System</h5>
+                            <p class="card-text">Aplikasi Sistem Multi Level Marketing</p>
+                            <p class=""><a href="https://sofin.id" target="_blank">Lihat</a></p>
+                        </div>
+                    </div>
+                </div>
+                <?php /** fixed asset */ ?>
+                <div class="col-4 mb-4">
+                    <div class="card h-100">
+                        <div id="carouselSnipeit" class="carousel slide" data-bs-touch="false" data-bs-interval="false">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="images/snipe-it/snipe-it-dahsboard.png" class="d-block w-100">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="images/snipe-it/snipe-it-menu-assets.png" class="d-block w-100">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="images/snipe-it/snipe-it-scanner-audit.png" class="d-block w-100">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="images/snipe-it/snipe-it-item-info.png" class="d-block w-100">
+                                </div>
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselSnipeit" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselSnipeit" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Cis Opname</h5>
+                            <p class="card-text">Aplikasi android untuk stock opname inventaris asset</p>
+                            <p class=""><a href="#" target="">#</a></p>
+                        </div>
+                    </div>
+                </div>
+                <?php /** geo leibel */ ?>
+                <div class="col-4 mb-4">
+                    <div class="card h-100">
+                        <div id="carouselGeoleibel" class="carousel slide" data-bs-touch="false" data-bs-interval="false">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="images/geoleibel/geoleibel-mobile-1.png" class="d-block w-100">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="images/geoleibel/geoleibel-mobile-2.png" class="d-block w-100">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="images/geoleibel/geoleibel.png" class="d-block w-100">
+                                </div>
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselGeoleibel" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselGeoleibel" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Geo Leibel</h5>
+                            <p class="card-text">Aplikasi android untuk sales dalam melakukan pemetaan data lokasi customer</p>
+                            <p class=""><a href="#" target="">#</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- script js -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </body>
+</html>
